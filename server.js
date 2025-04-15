c7b297e912a704498b9e73f73795ba1e15e0f03b
Delphine2411 <kpankpand@gmail.com>
2025-04-15 14:30:27 +0100

    [ADD]:dependencies

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const bodyParser = require('body-parser');
 const app = express()
 const PORT = 5000
 const connectDB = require("./config/db");
-
+const { userAuth } = require("./middleware/auth.js");
 
 //Connecting the Database
 connectDB();
 app.use(express.json())
 app.use(bodyParser.json());
 app.use(cookieParser());
-
+app.get("/basic", userAuth, (req, res) => res.send("User Route"));
 const server = app.listen(PORT, () =>
     console.log(`Server Connected to port ${PORT}`)
   )
